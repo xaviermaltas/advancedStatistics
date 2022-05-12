4c22fb2bb9302a5f03dd24079d52557a6b502374
Xavier Maltas <xavi.maltass@gmail.com>
2022-05-13 00:46:45 +0200

    [A3] test

--- A3/code.R
+++ A3/code.R
@@ -2,134 +2,79 @@
 
 if (!require('dplyr')) install.packages('dplyr'); library('dplyr')
 if (!require('ggplot2')) install.packages('ggplot2'); library('ggplot2')
+if (!require('logr')) install.packages('logr'); library('logr')
 
 # Lectura del fitxer de dades
 df.raw <- read.csv("dat_Air_Stations.csv", sep=",")
-head(df.raw)
+# head(df.raw)
+
 
 # Regressió lineal
 ## Estudi comparatiu entre estacions
-
-df.split<-split(df.raw, df.raw$Estacion)
-
-orderByFechaPeriodo<-function(df){
-  df<-df[
-    with(df, order(Fecha, Periodo)),
-  ]
-  return(df)
-}
 contaminants <- c("PM10", "O3", "NO2", "SO2")
 ids <- c("1","2","3","10")
 
-# maxMeanContaminants <- function(largeList, names){
-#   newLL<-rep(NA,length(largeList))
-#   for(i in 1:length(largeList)){
-#     for(j in 1:length(names)){
-#       newLL[i].mean.names[j] <- aggregate(largeList[i]$'names[j]', list(largeList[i]$Fecha), FUN=mean, na.rm=TRUE) 
-#       newLL[i].max.names[j] <- aggregate(largeList[i]$'names[j]', list(largeList[i]$Fecha), FUN=max, na.rm=TRUE)
-#     }
-#   }
-# }
+df.split<-split(df.raw, df.raw$Estacion) #split by 'Estacion'
+df.split[5]
 
-df.1 <- orderByFechaPeriodo(df.raw[df.raw$Estacion == 1, ])
-df.1.PM10.mean <- aggregate(df.1$PM10, list(df.1$Fecha), FUN=mean, na.rm=TRUE)  #https://www.statology.org/r-mean-by-group/
-df.1.PM10.max <- aggregate(df.1$PM10, list(df.1$Fecha), FUN=max, na.rm=TRUE)
-df.1.O3.mean <- aggregate(df.1$O3, list(df.1$Fecha), FUN=mean, na.rm=TRUE)
-df.1.O3.max <- aggregate(df.1$O3, list(df.1$Fecha), FUN=max, na.rm=TRUE)
-df.1.NO2.mean <- aggregate(df.1$NO2, list(df.1$Fecha), FUN=mean, na.rm=TRUE)
-df.1.NO2.max <- aggregate(df.1$NO2, list(df.1$Fecha), FUN=max, na.rm=TRUE)
-df.1.SO2.mean <- aggregate(df.1$SO2, list(df.1$Fecha), FUN=mean, na.rm=TRUE)
-df.1.SO2.max <- aggregate(df.1$SO2, list(df.1$Fecha), FUN=max, na.rm=TRUE)
-
-
-df.2 <- orderByFechaPeriodo(df.raw[df.raw$Estacion == 2, ])
-df.2.PM10.mean <- aggregate(df.2$PM10, list(df.2$Fecha), FUN=mean, na.rm=TRUE)
-df.2.PM10.max <- aggregate(df.2$PM10, list(df.2$Fecha), FUN=max, na.rm=TRUE)
-df.2.O3.mean <- aggregate(df.2$O3, list(df.2$Fecha), FUN=mean, na.rm=TRUE)
-df.2.O3.max <- aggregate(df.2$O3, list(df.2$Fecha), FUN=max, na.rm=TRUE)
-df.2.NO2.mean <- aggregate(df.2$NO2, list(df.2$Fecha), FUN=mean, na.rm=TRUE)
-df.2.NO2.max <- aggregate(df.2$NO2, list(df.2$Fecha), FUN=max, na.rm=TRUE)
-df.2.SO2.mean <- aggregate(df.2$SO2, list(df.2$Fecha), FUN=mean, na.rm=TRUE)
-df.2.SO2.max <- aggregate(df.2$SO2, list(df.2$Fecha), FUN=max, na.rm=TRUE)
-
-
-df.3 <- orderByFechaPeriodo(df.raw[df.raw$Estacion == 3, ])
-df.3.PM10.mean <- aggregate(df.3$PM10, list(df.3$Fecha), FUN=mean, na.rm=TRUE)
-df.3.PM10.max <- aggregate(df.3$PM10, list(df.3$Fecha), FUN=max, na.rm=TRUE)
-df.3.O3.mean <- aggregate(df.3$O3, list(df.3$Fecha), FUN=mean, na.rm=TRUE)
-df.3.O3.max <- aggregate(df.3$O3, list(df.3$Fecha), FUN=max, na.rm=TRUE)
-df.3.NO2.mean <- aggregate(df.3$NO2, list(df.3$Fecha), FUN=mean, na.rm=TRUE)
-df.3.NO2.max <- aggregate(df.3$NO2, list(df.3$Fecha), FUN=max, na.rm=TRUE)
-df.3.SO2.mean <- aggregate(df.3$SO2, list(df.3$Fecha), FUN=mean, na.rm=TRUE)
-df.3.SO2.max <- aggregate(df.3$SO2, list(df.3$Fecha), FUN=max, na.rm=TRUE)
-
-
-df.4 <- orderByFechaPeriodo(df.raw[df.raw$Estacion == 4, ])
-df.4.PM10.mean <- aggregate(df.4$PM10, list(df.4$Fecha), FUN=mean, na.rm=TRUE)
-df.4.PM10.max <- aggregate(df.4$PM10, list(df.4$Fecha), FUN=max, na.rm=TRUE)
-df.4.O3.mean <- aggregate(df.4$O3, list(df.4$Fecha), FUN=mean, na.rm=TRUE)
-df.4.O3.max <- aggregate(df.4$O3, list(df.4$Fecha), FUN=max, na.rm=TRUE)
-df.4.NO2.mean <- aggregate(df.4$NO2, list(df.4$Fecha), FUN=mean, na.rm=TRUE)
-df.4.NO2.max <- aggregate(df.4$NO2, list(df.4$Fecha), FUN=max, na.rm=TRUE)
-df.4.SO2.mean <- aggregate(df.4$SO2, list(df.4$Fecha), FUN=mean, na.rm=TRUE)
-df.4.SO2.max <- aggregate(df.4$SO2, list(df.4$Fecha), FUN=max, na.rm=TRUE)
-
-
-df.10 <- orderByFechaPeriodo(df.raw[df.raw$Estacion == 10, ])
-df.10.PM10.mean <- aggregate(df.10$PM10, list(df.10$Fecha), FUN=mean, na.rm=TRUE)
-df.10.PM10.max <- aggregate(df.10$PM10, list(df.10$Fecha), FUN=max, na.rm=TRUE)
-df.10.O3.mean <- aggregate(df.10$O3, list(df.10$Fecha), FUN=mean, na.rm=TRUE)
-df.10.O3.max <- aggregate(df.10$O3, list(df.10$Fecha), FUN=max, na.rm=TRUE)
-df.10.NO2.mean <- aggregate(df.10$NO2, list(df.10$Fecha), FUN=mean, na.rm=TRUE)
-df.10.NO2.max <- aggregate(df.10$NO2, list(df.10$Fecha), FUN=max, na.rm=TRUE)
-df.10.SO2.mean <- aggregate(df.10$SO2, list(df.10$Fecha), FUN=mean, na.rm=TRUE)
-df.10.SO2.max <- aggregate(df.10$SO2, list(df.10$Fecha), FUN=max, na.rm=TRUE)
-
-plotMax <- function(names,ids){
-  for(i in 1:length(names)){
-    concatNames <-rep(NA,length(names))
-    dfMean$i <- df
-    
-    AllDfMean <- rbind(paste0("df.1.",names[i],".mean", sep=""),paste0("df.2.", names[i],".mean", sep=""),paste0("df.3.",names[i],".mean", sep=""),paste0("df.4.",names[i],".mean", sep=""),paste0("df.10.",names[i],".mean", sep=""))
-    View(AllDfMean)
-    AllDfMax <- rbind(paste0("df.1.",i,".mean", sep=""),paste0("df.2.",i,".mean", sep=""),paste0("df.3.",i,".mean", sep=""),paste0("df.4.",i,".mean", sep=""),paste0("df.10.",i,".mean", sep=""))
+maxMeanContaminants <- function(largeList, names){
+     
+  newLargeList <- list()
+       
+  for( i in 1:length(largeList)){
+  
+    # mat = matrix(NA,ncol = (length(names)*2), nrow = 365)
+    # df_target=data.frame(mat)
+  
+    mat = matrix(ncol=1, nrow=365)
+    tmp_list <- rep(list(mat),8)
+  
+    for( j in 1:length(names)){
+      currentList<-largeList[i]
+      currentName <- names[j]
+  
+      newMeanName <- paste(currentName,'.mean', sep="" )
+      newMaxName <- paste(currentName,'.mean', sep="" )
+  
+      computedMeanList<- aggregate(currentList[[1]][currentName], list(currentList[[1]]$Fecha), FUN=mean, na.rm=TRUE )
+      computedMaxList<- aggregate(currentList[[1]][currentName], list(currentList[[1]]$Fecha), FUN=max, na.rm=TRUE )
+  
+      meanList <- list(computedMeanList[2])
+      maxList <- list(computedMaxList[2])
+  
+      # newDF$newMeanName <- cbind(newDF,computedMeanList[2])
+      # newDF$newMaxName <- cbind(newDF,computedMaxList[2])
+      
+      # newDF[ , ncol(newDF) + 1] <- computedMeanList[2]
+      # newDF[ , ncol(newDF) + 1] <- computedMaxList[2]
+      
+      # tmp_list[[(j)]]<- computedMeanList[2]
+      # tmp_list[[(j)+1]]<- computedMaxList[2]
+      # print(tmp_list)
+      
+      tmp_list[[j]] <- meanList
+      tmp_list[[j+1]] <- maxList
+    }
+    print(tmp_list)
+    newDF <- data.frame(tmp_list)
+    newLargeList[i] <- newDF
   }
+  return(newLargeList)
 }
-plotMax(contaminants, ids)
 
-paste0("age.",years[1])
-
-tes <- function(ids){
-  for(i in 1:length(ids)){
-    df$ids[i] <- df.ids[i].PM10.mean$x
-  }
-}
-df.PM10.mean <- data.frame()
-df.PM10.mean['new'] <- df.1.PM10.mean$x
-# , df.2.PM10.mean$x, df.3.PM10.mean$x, df.4.PM10.mean$x, df.10.PM10.mean$x
-
-ggplot(data=df.PM10.mean)
-
-graph.PM10.mean <- lapply(df.PM10.mean,function(x)
-  p<-ggplot(x) +
-    geom_line() + 
-    facet_wrap(~col1)
-)
-print (graph.PM10.mean)
-
-
- 
-
-## Model de regressió lineal
-## Model de regressió lineal múltiple
-## Diagnosi del model
-## Predicció del model
-
-# Regressó logística
-## Anàlisi cru de possibles factors de risc. Cáculo de OR
-## Model de regressió logística
-## Predicció
-## Bondat de l'ajust
-## Corba ROC
-
-# Conclusió de l'anàlisi
+maxMeanLargeList <- maxMeanContaminants(df.split, contaminants)
+# 
+# con <- file("test.log")
+# sink(con, append=TRUE)
+# sink(con, append=TRUE, type="message")
+# 
+# # This will echo all input and not truncate 150+ character lines...
+# source("code.R", echo=TRUE, max.deparse.length=10000)
+# 
+# # Restore output to console
+# sink() 
+# sink(type="message")
+# 
+# # And look at the log...
+# cat(readLines("test.log"), sep="\n")
+# 
